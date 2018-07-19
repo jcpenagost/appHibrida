63377c899ce6016ca6428341aef2b96c53c945ca
Sala 502 <sala502@Laboratorios.net>
2018-07-18 19:56:57 -0500

    cambio LISTA

--- src/pages/list/list.ts
+++ src/pages/list/list.ts
@@ -19,8 +19,8 @@ export class ListPage {
     this.items = [];
     for(let i = 1; i < 11; i++) {
       this.items.push({
-        title: 'Item ' + i,
-        note: 'This is item #' + i,
+        title: 'JC - Item ' + i,
+        note: 'JC - This is item #' + i,
         icon: this.icons[Math.floor(Math.random() * this.icons.length)]
       });
     }
