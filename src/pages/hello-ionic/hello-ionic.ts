bfbb05eb9958990fb9bd23278d9ae5bd7db8ed85
Sala 502 <sala502@Laboratorios.net>
2018-07-25 21:27:42 -0500

    link

--- src/pages/hello-ionic/hello-ionic.ts
+++ src/pages/hello-ionic/hello-ionic.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
+import { ListPage} from '../list/list';
 
 @Component({
   selector: 'page-hello-ionic',
   templateUrl: 'hello-ionic.html'
 })
 export class HelloIonicPage {
-  constructor() {
 
+  pagina1=ListPage;
+/*   pagina2=
+  pagina3=
+  pagina4= */
+
+  constructor(
+    private navctrl:NavController,
+    private navparams:NavParams) {
+
+  }
+
+  irPagina(pagina){
+    this.navctrl.push(pagina);
   }
 }
